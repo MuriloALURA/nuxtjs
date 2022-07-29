9cd272da296438cd8e2038671627750bc7112205
Murilo <murilo.alvess@yahoo.com>
2022-07-29 14:35:57 -0300

    feat: 🛠 filtragem de pesquisa e edição de valores

--- projeto-nuxtjs/store/transactions/actions.js
+++ projeto-nuxtjs/store/transactions/actions.js
@@ -1,11 +1,16 @@
 export default {
-    getTransactions() {
-      return this.$axios.$get('transactions?_expand=category');
-    },
-    addTransaction(context, data) {
-      return this.$axios.$post('transactions', data);
-    },
-    updateTransaction(context, { id, data }) {
-      return this.$axios.$patch(`transactions/${id}`, data);
-    },
-  }
+  getTransactions(context, filter = {}) {
+    return this.$axios.$get('transactions', {
+      params: {
+        _expand: 'category',
+        ...filter
+      }
+    });
+  },
+  addTransaction(context, data) {
+    return this.$axios.$post('transactions', data);
+  },
+  updateTransaction(context, { id, data }) {
+    return this.$axios.$patch(`transactions/${id}`, data);
+  },
+}
